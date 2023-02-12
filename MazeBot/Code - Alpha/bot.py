905bf09dc9fc497705d93fcf0f99de7331c5708c
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-02-12 20:28:54 +0800

    Change name

---
+++ MazeBot/Code - Alpha/bot.py
@@ -0,0 +1,7 @@
+import utilities as utils
+
+def a_star():
+    return None
+
+def heuristic():
+    return None
--- MazeBot/Code - Alpha/logic.py
+++
@@ -1,8 +0,0 @@
-def find_manhattan(xi:int, yi:int, xf:int, yf:int):
-    return abs(xf-xi) + abs(yf-yi)
-
-def a_star():
-    return None
-
-def heuristic():
-    return None
